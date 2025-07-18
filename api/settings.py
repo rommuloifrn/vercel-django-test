a1507cf47ee868d56c0e5d283a549a32913e3790
rommuloifrn <roomulo.cavalkant@outlook.com>
2025-07-17 22:18:33 -0300

    build: add postgres integration

--- api/settings.py
+++ api/settings.py
@@ -11,10 +11,15 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os, environ
+
+env = environ.Env(
+)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -76,7 +81,9 @@ WSGI_APPLICATION = 'api.wsgi.app'
 # Note: Django modules for using databases are not support in serverless
 # environments like Vercel. You can use a database over HTTP, hosted elsewhere.
 
-DATABASES = {}
+DATABASES = {
+    'default': env.db(),
+}
 
 
 # Password validation
